2b828258e6202a54bc32e931fa8a1ba50841740c
gerijeb <gtpepi@hotmail.com>
2022-01-10 11:27:59 -0300

    Fix held down key bug

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
+let justPlayed = null;
+
 function playAudio (n) {
     const audio = document.querySelector(`audio[data-key="${n.keyCode}"]`);
     const key = document.querySelector(`.key[data-key="${n.keyCode}"]`)
+
+    if (n.keyCode === justPlayed) return;
 
     if (!audio) return;
     audio.currentTime=0;
     audio.play();
     key.classList.add('playing');
+    justPlayed = n.keyCode;
 }
 
 function removeTransition(n) {
@@ -17,7 +22,7 @@ const keys = document.querySelectorAll('.key');
 keys.forEach(key => key.addEventListener ('transitionend', removeTransition));
 
 window.addEventListener('keydown', playAudio);
-
+window.addEventListener('keyup', () => justPlayed = null);
 
 
 
